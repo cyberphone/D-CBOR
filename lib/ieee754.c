a2341d0949b9044d7d109b90b0d4068fb9853b0a
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-02-28 04:52:52 +0100

    goto everywhere, ugly but logical

--- lib/ieee754.c
+++ lib/ieee754.c
@@ -1,4 +1,4 @@
-// Determinstic CBOR IEEE-754 support
+// Determinstic CBOR IEEE-754 encoder support
 
 #include <string.h>
 
@@ -36,133 +36,138 @@ const int MT_FLOAT16 = 0xf9;
 const int MT_FLOAT32 = 0xfa;
 const int MT_FLOAT64 = 0xfb;
 
-void addDouble(CBOR_BUFFER *cborBuffer, double value) {
+void addDouble(CBOR_BUFFER *cborBuffer, double d) {
 
-    // Initial assumption: value is a plain vanilla 64-bit double.
+    // Initial assumption: the number is a plain vanilla 64-bit double.
     int tag = MT_FLOAT64;
     uint64_t bitFormat;
-    memcpy(&bitFormat, &value, sizeof(uint64_t));
+    memcpy(&bitFormat, &d, sizeof(uint64_t));
 
     // Check for possible edge cases.
+
     if ((bitFormat & ~FLOAT64_NEG_ZERO) == FLOAT64_POS_ZERO) {
-        // Some zeroes are more zero than others :)
+        // Some zeroes are apparently more zero than others...
         tag = MT_FLOAT16;
         bitFormat = (bitFormat == FLOAT64_POS_ZERO) ? FLOAT16_POS_ZERO : FLOAT16_NEG_ZERO;
-    } else if ((bitFormat & FLOAT64_POS_INFINITY) == FLOAT64_POS_INFINITY) {
+        goto done;  // Ever heard about "goto free" programming? Yes, it sucks :)
+    }
+
+    if ((bitFormat & FLOAT64_POS_INFINITY) == FLOAT64_POS_INFINITY) {
         // Special "number".
         tag = MT_FLOAT16;
         bitFormat = (bitFormat == FLOAT64_POS_INFINITY) ?
             FLOAT16_POS_INFINITY : (bitFormat == FLOAT64_NEG_INFINITY) ?
                 // Deterministic representation of NaN => No NaN "signaling".
                 FLOAT16_NEG_INFINITY : FLOAT16_NOT_A_NUMBER;
-    } else {
-        // It is apparently a regular number. Does it fit in a 32-bit float?
-
-#ifndef PLATFORM_SUPPORTS_FLOAT_CAST
-        int64_t exponent = ((bitFormat >> FLOAT64_SIGNIFICAND_SIZE) &
-            ((ONE << FLOAT64_EXPONENT_SIZE) - 1)) -
-            (FLOAT64_EXPONENT_BIAS - FLOAT32_EXPONENT_BIAS);
-        if (exponent > ((int64_t)FLOAT32_EXPONENT_BIAS << 1)) {
-            // Too big for float32 or into the space reserved for NaN and Infinity.
-            goto generate;
-        }
-
-        uint64_t significand = bitFormat & ((ONE << FLOAT64_SIGNIFICAND_SIZE) - 1);
-        if ((significand &
-            ((ONE << (FLOAT64_SIGNIFICAND_SIZE - FLOAT32_SIGNIFICAND_SIZE)) - 1)) != 0) {
-            // Losing significand bits is not an option.
-            goto generate;
-        }
-        significand >>= (FLOAT64_SIGNIFICAND_SIZE - FLOAT32_SIGNIFICAND_SIZE);
-
-        // Check if we need to denormalize data.
-        if (exponent <= 0) {
-            // The implicit "1" becomes explicit using subnormal representation.
-            significand += ONE << FLOAT32_SIGNIFICAND_SIZE;
-            exponent--;
-            // Always perform at least one turn.
-            do {
-                if ((significand & 1) != 0) {
-                    // Too off scale for float32.
-                    // This test also catches subnormal float64 numbers.
-                    goto generate;
-                }
-                significand >>= 1;
-            } while (++exponent < 0);
-        }
-#else
-        float floatValue = (float)value;
-        if (value != floatValue) {
-            // After casting to float32 something got lost so we stick to float64.
-            goto generate;
-        }
-#endif
-
-        // Yes, the number is compatible with 32-bit float representation.
-        tag = MT_FLOAT32;
-#ifndef PLATFORM_SUPPORTS_FLOAT_CAST
-        bitFormat =
-            // Put sign bit in position.
-            ((bitFormat >> (64 - 32)) & FLOAT32_NEG_ZERO) +
-            // Exponent.  Put it in front of significand.
-            (exponent << FLOAT32_SIGNIFICAND_SIZE) +
-            // Significand.
-            significand;
-#else
-        uint32_t floatBinary;
-        memcpy(&floatBinary, &floatValue, sizeof(float));
-        bitFormat = floatBinary;
-#endif
-
-        // However, we must still check if the number could fit in a 16-bit float.
-#ifdef PLATFORM_SUPPORTS_FLOAT_CAST
-        int64_t exponent = ((bitFormat >> FLOAT32_SIGNIFICAND_SIZE) &
-            ((ONE << FLOAT32_EXPONENT_SIZE) - 1)) -
-            (FLOAT32_EXPONENT_BIAS - FLOAT16_EXPONENT_BIAS);
-#else
-        exponent -= (FLOAT32_EXPONENT_BIAS - FLOAT16_EXPONENT_BIAS);
-#endif
-        if (exponent > ((int64_t)FLOAT16_EXPONENT_BIAS << 1)) {
-            // Too big for float16 or into the space reserved for NaN and Infinity.
-            goto generate;
-        }
-
-#ifdef PLATFORM_SUPPORTS_FLOAT_CAST
-        uint64_t significand = bitFormat & ((ONE << FLOAT32_SIGNIFICAND_SIZE) - 1);
-#endif
-        if ((significand &
-            ((ONE << (FLOAT32_SIGNIFICAND_SIZE - FLOAT16_SIGNIFICAND_SIZE)) - 1)) != 0) {
-            // Losing significand bits is not an option.
-            goto generate;
-        }
-        significand >>= (FLOAT32_SIGNIFICAND_SIZE - FLOAT16_SIGNIFICAND_SIZE);
-
-        // Check if we need to denormalize data.
-        if (exponent <= 0) {
-            // The implicit "1" becomes explicit using subnormal representation.
-            significand += ONE << FLOAT16_SIGNIFICAND_SIZE;
-            exponent--;
-            // Always perform at least one turn.
-            do {
-                if ((significand & 1) != 0) {
-                    // Too off scale for float16.
-                    // This test also catches subnormal float32 numbers.
-                    goto generate;
-                }
-                significand >>= 1;
-            } while (++exponent < 0);
-        }
-
-        // Seems like 16 bits indeed are sufficient!
-        tag = MT_FLOAT16;
-        bitFormat =
-            // Put sign bit in position.
-            ((bitFormat >> (32 - 16)) & FLOAT16_NEG_ZERO) +
-            // Exponent.  Put it in front of significand.
-            (exponent << FLOAT16_SIGNIFICAND_SIZE) +
-            // Significand.
-            significand;
+        goto done;
     }
-generate:
+
+    // It is apparently a regular number. Does it fit in a 32-bit float?
+
+#ifndef PLATFORM_SUPPORTS_FLOAT_CAST
+    int64_t exponent = 
+        ((bitFormat >> FLOAT64_SIGNIFICAND_SIZE) & ((ONE << FLOAT64_EXPONENT_SIZE) - 1)) -
+        (FLOAT64_EXPONENT_BIAS - FLOAT32_EXPONENT_BIAS);
+    if (exponent > ((int64_t)FLOAT32_EXPONENT_BIAS << 1)) {
+        // Too big for float32 or into the space reserved for NaN and Infinity.
+        goto done;
+    }
+
+    uint64_t significand = bitFormat & ((ONE << FLOAT64_SIGNIFICAND_SIZE) - 1);
+    if ((significand & ((ONE << (FLOAT64_SIGNIFICAND_SIZE - FLOAT32_SIGNIFICAND_SIZE)) - 1)) != 0) {
+        // Losing significand bits is not an option.
+        goto done;
+    }
+    significand >>= (FLOAT64_SIGNIFICAND_SIZE - FLOAT32_SIGNIFICAND_SIZE);
+
+    // Check if result needs to be denormalized.
+    if (exponent <= 0) {
+        // The implicit "1" becomes explicit using subnormal representation.
+        significand += ONE << FLOAT32_SIGNIFICAND_SIZE;
+        exponent--;
+        // Always perform at least one turn.
+        do {
+            if ((significand & 1) != 0) {
+                // Too off scale for float32.
+                // This test also catches subnormal float64 numbers.
+                goto done;
+            }
+            significand >>= 1;
+        } while (++exponent < 0);
+    }
+#else
+    float f = (float)d;
+    if (d != f) {
+        // After casting to float32 something got lost.  Stick to float64.
+        goto done;
+    }
+#endif
+
+    // Yes, the number is compatible with 32-bit float representation.
+    tag = MT_FLOAT32;
+#ifndef PLATFORM_SUPPORTS_FLOAT_CAST
+    bitFormat =
+        // Put sign bit in position.
+        ((bitFormat >> (64 - 32)) & FLOAT32_NEG_ZERO) +
+        // Exponent.  Put it in front of significand.
+        (exponent << FLOAT32_SIGNIFICAND_SIZE) +
+        // Significand.
+        significand;
+#else
+    uint32_t floatBinary;
+    memcpy(&floatBinary, &f, sizeof(float));
+    bitFormat = floatBinary;
+#endif
+
+    // However, the number could potentially fit in a 16-bit float as well.
+
+#ifdef PLATFORM_SUPPORTS_FLOAT_CAST
+    int64_t exponent = ((bitFormat >> FLOAT32_SIGNIFICAND_SIZE) &
+        ((ONE << FLOAT32_EXPONENT_SIZE) - 1)) -
+        (FLOAT32_EXPONENT_BIAS - FLOAT16_EXPONENT_BIAS);
+#else
+    exponent -= (FLOAT32_EXPONENT_BIAS - FLOAT16_EXPONENT_BIAS);
+#endif
+    if (exponent > ((int64_t)FLOAT16_EXPONENT_BIAS << 1)) {
+        // Too big for float16 or into the space reserved for NaN and Infinity.
+        goto done;
+    }
+
+#ifdef PLATFORM_SUPPORTS_FLOAT_CAST
+    uint64_t significand = bitFormat & ((ONE << FLOAT32_SIGNIFICAND_SIZE) - 1);
+#endif
+    if ((significand & ((ONE << (FLOAT32_SIGNIFICAND_SIZE - FLOAT16_SIGNIFICAND_SIZE)) - 1)) != 0) {
+        // Losing significand bits is not an option.
+        goto done;
+    }
+    significand >>= (FLOAT32_SIGNIFICAND_SIZE - FLOAT16_SIGNIFICAND_SIZE);
+
+    // Check if result needs to be denormalized.
+    if (exponent <= 0) {
+        // The implicit "1" becomes explicit using subnormal representation.
+        significand += ONE << FLOAT16_SIGNIFICAND_SIZE;
+        exponent--;
+        // Always perform at least one turn.
+        do {
+            if ((significand & 1) != 0) {
+                // Too off scale for float16.
+                // This test also catches subnormal float32 numbers.
+                goto done;
+            }
+            significand >>= 1;
+        } while (++exponent < 0);
+    }
+
+    // Seems like 16 bits indeed are sufficient!
+    tag = MT_FLOAT16;
+    bitFormat =
+        // Put sign bit in position.
+        ((bitFormat >> (32 - 16)) & FLOAT16_NEG_ZERO) +
+        // Exponent.  Put it in front of significand.
+        (exponent << FLOAT16_SIGNIFICAND_SIZE) +
+        // Significand.
+        significand;
+
+done:
     encodeTagAndValue(cborBuffer, tag, 2 << (tag - MT_FLOAT16), bitFormat);
 }

b0890bd97df65a4204d968d6e4199d33efc58be5
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-03-01 13:18:57 +0100

    Create build-and-run.sh

---
+++ d-cbor-4-constrained-device/build-and-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Rudimentary Linux bash script for building and running the D-CBOR demo.
+pushd $(dirname "${BASH_SOURCE[0]}")
+gcc -o demo -Os -DPLATFORM_SUPPORTS_FLOAT_CAST -I ../lib d-cbor-4-constrained-device.c ../lib/*.c
+./demo
+popd
+

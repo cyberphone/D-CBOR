e1ba70cfabb5bdac40fb35f885cfbd266836b917
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-03-01 13:39:38 +0100

    More comments

--- d-cbor-4-constrained-device/build-and-run.bash
+++ d-cbor-4-constrained-device/build-and-run.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Rudimentary Linux bash script for building and running the D-CBOR demo.
+#
+# Run from any directory: $ bash path-to-this-script
+#
 pushd $(dirname "${BASH_SOURCE[0]}")
 gcc -o demo -Os -DPLATFORM_SUPPORTS_FLOAT_CAST -I ../lib d-cbor-4-constrained-device.c ../lib/*.c
 ./demo

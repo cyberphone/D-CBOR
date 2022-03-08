16e5b46efc16961c1377c6e26be9e1cb8171e0ca
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-03-08 17:24:38 +0100

    Update print-buffer.c

--- verify-demo/print-buffer.c
+++ verify-demo/print-buffer.c
@@ -5,8 +5,9 @@
 #include "print-buffer.h"
 
 void printUsefulBufC(UsefulBufC *usefulBufC, char *string) {
-    printf("%s [%d]:\n", string, usefulBufC->len);
-    for (size_t i = 0; i < usefulBufC->len; i++) {
+    int length = (int)usefulBufC->len;
+    printf("%s [%d]:\n", string, length);
+    for (int i = 0; i < length; i++) {
         printf("%02x", ((uint8_t*)usefulBufC->ptr)[i]);
     }
     printf("\n\n");
